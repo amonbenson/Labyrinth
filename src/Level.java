4d98a0f117c738c41e3ccea7013224826f282e8a
lucj26 <jungnickel.luca@gmail.com>
2017-11-04 22:27:51 +0100

    Added level moveUp

--- src/Level.java
+++ src/Level.java
@@ -17,6 +17,30 @@ public class Level {
 		
 	}
 	
+	//Bewegt eine bestimmte MoveableStone Gruppe nach oben
+	public void moveUp(int ID) {
+		
+		for (int x = 0;x<width;x++) { 
+			for (int y = 1;y<height;y++) { //y = 0 kann nicht nach oben bewegt werden
+				if (field[x][y] instanceof StoneMoveable) {
+					
+					StoneMoveable sm = (StoneMoveable) field[x][y];
+					
+					if (sm.ID == ID) {
+						if (field[x][y-1] == null ) {
+							field[x][y-1] = field[x][y];
+							field[x][y] = null;
+						}
+					}
+				}
+
+			}
+		}
+		
+	}
 	
+	public void moveLeft(int ID) {
+		
+	}
 	
 }
