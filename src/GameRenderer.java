a537df50b08a4d094aaa05b1b19735a382b75f57
lucj26 <jungnickel.luca@gmail.com>
2017-11-04 22:16:45 +0100

    Changed GameRenderer

--- src/GameRenderer.java
+++ src/GameRenderer.java
@@ -4,6 +4,8 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class GameRenderer {
 	
+	public static final int TILE_SIZE;
+	
 	public void render(GameContainer gc, StateBasedGame sbg, Game game, Graphics g) {
 		renderTiles(gc, sbg, game, g);
 	}
@@ -11,7 +13,16 @@ public class GameRenderer {
 	
 	
 	private void renderTiles(GameContainer gc, StateBasedGame sbg, Game game, Graphics g) {
-		
+		for (int x = 0;x<game.level.width;x++) {
+			for (int y = 0;y<game.level.height;y++) {
+				if (game.level.field[x][y] != null) { //Game Tiles können auch null sein
+					Tile t = game.level.field[x][y];
+					if (t instanceof Stone) {
+						
+					}
+				}
+			}
+		}
 	}
 	
 }
